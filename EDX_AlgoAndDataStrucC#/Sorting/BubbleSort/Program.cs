fac9712f27ee5eb9f6994a03b6b0caecc26e3c36
Nandu Anil <nandua.dev@gmail.com>
2018-07-09 16:53:31 -0700

    Bubble and Insertion Sort

---
+++ EDX_AlgoAndDataStrucC#/Sorting/BubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5,10,3,2,4 };
+            var swap = false;
+            var count = nums.Length-1;
+            do
+            {
+                swap = false;
+                for(int i =0; i < count; i++ )
+                {
+                    if(nums[i]>nums[i+1])
+                    {
+                        var temp = nums[i];
+                        nums[i] = nums[i+1];
+                        nums[i+1] = temp;
+                        swap = true;
+                    }
+                } 
+                count--;
+            }while(swap == true);
+
+            nums.ToList().ForEach(x => {Console.WriteLine(x);});
+            foreach(var num in nums){Console.WriteLine(num);}
+        }
+    }
+}
---
+++ EDX_AlgoAndDataStrucC#/Sorting/InsertionSort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 7,8,4,6,2,1 };
+
+            for(int i = 1; i<arr.Length; i++)
+            {
+                var currentValue = arr[i];
+                int j = i;
+                while(j>0 && currentValue < arr[j-1])
+                {
+                    arr[j] = arr[j-1];
+                    j--;
+                }
+                arr[j] = currentValue;
+            }
+
+            foreach(var o in arr){Console.WriteLine(o);}
+        }
+    }
+}
